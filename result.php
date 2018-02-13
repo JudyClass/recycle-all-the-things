39ca9f6dd3ac2ad03ef353bcfab9ce1faca66750
Judy <jampafoo@gmail.com>
2018-02-13 12:06:54 -0600

    adding project files

---
+++ result.php
@@ -0,0 +1,73 @@
+<?php include 'header.php'; ?>
+
+
+ 
+
+    <!-- Navigation -->
+<!--    <nav class="navbar navbar-light bg-light static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Recy</a>
+        <a class="btn btn-primary" href="#">Sign In</a>
+      </div>
+    </nav>
+-->
+    <!-- Masthead -->
+    <header class="page-masthead text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <h1 class="mb-5">Is it Recyclable?</h1>
+          </div>
+          <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+            <form>
+              <div class="form-row">
+                <div class="col-12 col-md-9 mb-2 mb-md-0">
+                  <input type="text" class="form-control form-control-lg" placeholder="Candy Wrapper">
+                </div>
+                <div class="col-12 col-md-3">
+                  <button type="submit" class="btn btn-block btn-lg btn-primary">Check it!</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+
+    <!-- Image Showcases -->
+   <section class="showcase">
+      <div class="container-fluid p-0">
+     
+        <div class="row no-gutters">
+	        <div class="col-lg-6 my-auto showcase-text order-lg-2">
+            <h2>Yes! (Or NO)</h2>
+            <p class="lead mb-0">(SEARCH TERM) is Recycleable!</p>
+            <p>This is how you handle it for your area facilities</p>
+          </div>
+          <div class="col-lg-6 text-white showcase-img order-lg-1" style="background-image: url('img/plastic-bottles.jpg');"></div>
+          
+        </div>
+        
+      </div>
+    </section>
+     <section class="call-to-action text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <h2 class="mb-4">MEDIA</h2>
+            <p>Why we need to recycle (SEARCH TERM)</p>
+            <div class="embed-responsive embed-responsive-16by9">
+             <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/HQTUWK7CM-Y" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+<?php include 'footer.php'; ?>
+
